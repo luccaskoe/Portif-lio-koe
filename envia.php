679600d350a6516d97cf6b7e42b731809ac5a090
Lucca koe <lukakoe0014@gmail.com>
2025-08-24 19:07:52 -0400

    Update envia.php

--- envia.php
+++ envia.php
@@ -1,20 +1,17 @@
-
 <?php
-
     $nome = addslashes($_POST['nome']);
     $email = addslashes($_POST['email']);
     $telefone = addslashes($_POST['telefone']);
     $mensagem = addslashes($_POST['mensagem']);
+    
+   $destinatario = "koebyte001@gmail.com";
+   $assunto = "Coleta de dados - Inteliogia";
+  
+   $corpoemail = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."telefone: ".$telefone."mensagem: ".$mensagem;
 
+   $cabeca = "From: luakoe0014@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
 
-    $para = "koebyte0014@gmail.com";
-    $assunto = "Coleta de dados - Inteliogia";
-
-    $corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Telefone: ".$telefone. "\n"."Mensagem: ".$mensagem;
-
-    $cabeca = "From: lukakoe0014@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
-
-    if(mail($para,$assunto,$corpo,$cabeca)){
+    if (mail($destinatario, $assunto, $corpoemail, $cabeca)) {
         echo("E-mail enviado com sucesso!");
     }else{
         echo("Houve um erro ao enviar o email!");
